99682923e9f086b03375d379dfddd9c11bb78552
chinguyen0000 <lc.duoclang@gmail.com>
2024-09-26 22:20:00 +0700

    submit ss12 exercise

---
+++ src/s12_java_collection_framework/bai_tap/array_list/controller/ProductManager.java
@@ -0,0 +1,81 @@
+package s12_java_collection_framework.bai_tap.array_list.controller;
+
+import s12_java_collection_framework.bai_tap.array_list.model.Product;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class ProductManager implements Comparator<Product> {
+    private List<Product> productList = new ArrayList<>();
+
+    public ProductManager() {
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void addProduct(Product product) {
+        if (!productList.contains(product) && product != null) {
+            productList.add(product);
+            System.out.println("Add product: " + product.toString() + " success");
+        } else {
+            System.out.println("Add product: " + product.toString() + " failed");
+        }
+    }
+
+    public void removeProduct(int index) {
+        if (index >= 0 && index < productList.size()) {
+            Product removedProduct = productList.remove(index);
+            System.out.println("Remove product: " + removedProduct.toString() + " success");
+        } else {
+            System.out.println("Remove product failed");
+        }
+    }
+
+    public void updateProduct(int index) {
+        if (index >= 0 && index < productList.size()) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter new product name: ");
+            String newName = sc.nextLine();
+            productList.get(index).setName(newName);
+            System.out.println("Update product: " + productList.get(index).toString() + " success");
+        } else {
+            System.out.println("Update product failed");
+        }
+    }
+
+    public void displayProducts() {
+        for (Product product : productList) {
+            System.out.println(product.toString());
+        }
+    }
+
+    public void searchProduct(String productName) {
+        int count = 0;
+        for (Product product : productList) {
+            if (product.getName().equals(productName)) {
+                count++;
+                System.out.println(product.toString());
+            }
+        }
+        if (count > 0) {
+            System.out.println(count + " products found");
+        } else {
+            System.out.println("No product found");
+        }
+    }
+
+    public int compare(Product p1, Product p2) {
+        if (p1.getPrice() > p2.getPrice()) {
+            return 1;
+        } else if (p1.getPrice() == p2.getPrice()) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+}
---
+++ src/s12_java_collection_framework/bai_tap/linked_list/view/ProductDisplay.java
@@ -0,0 +1,65 @@
+package s12_java_collection_framework.bai_tap.linked_list.view;
+
+import s12_java_collection_framework.bai_tap.linked_list.controller.ProductManager;
+import s12_java_collection_framework.bai_tap.linked_list.model.Product;
+
+import java.util.Collections;
+import java.util.Scanner;
+
+public class ProductDisplay {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ProductManager pm = new ProductManager();
+        pm.addProduct(new Product(4, "Màn hình 2", 3500000));
+        pm.addProduct(new Product(3, "Màn hình", 5000000));
+        pm.addProduct(new Product(1, "Chuột", 350000));
+        pm.addProduct(new Product(2, "Bàn phím", 999000));
+
+
+        while (true) {
+            System.out.println("1. Display product list" + "\n2. Add product to list" + "\n3. Update product name with index" + "\n4. Delete product from list" + "\n5. Search product by name" + "\n6. Sort product by price in ascending order" + "\n7. Sort product by price in descending order" + "\n0. Exit");
+            int choice = Integer.parseInt(sc.nextLine());
+            switch (choice) {
+                case 1:
+                    pm.displayProducts();
+                    break;
+                case 2:
+                    System.out.println("Enter the id of product");
+                    int id = Integer.parseInt(sc.nextLine());
+                    System.out.println("Enter the name of product");
+                    String name = sc.nextLine();
+                    System.out.println("Enter the price of product");
+                    int price = Integer.parseInt(sc.nextLine());
+                    pm.addProduct(new Product(id, name, price));
+                    break;
+                case 3:
+                    System.out.println("Enter the position of product");
+                    int index = Integer.parseInt(sc.nextLine());
+                    pm.updateProduct(index);
+                    break;
+                case 4:
+                    System.out.println("Enter the position of product");
+                    index = Integer.parseInt(sc.nextLine());
+                    pm.removeProduct(index);
+                    break;
+                case 5:
+                    System.out.println("Enter the name of product");
+                    name = sc.nextLine();
+                    pm.searchProduct(name);
+                    break;
+                case 6:
+                    Collections.sort(pm.getProductList(),pm);
+                    pm.displayProducts();
+                    break;
+                case 7:
+                    Collections.sort(pm.getProductList(),pm);
+                    Collections.reverse(pm.getProductList());
+                    pm.displayProducts();
+                    break;
+                case 0:
+                default:
+                    System.exit(0);
+            }
+        }
+    }
+}
